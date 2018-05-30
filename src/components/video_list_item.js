49495040d794954018e758feff6b8d492a9a05c1
Johnnypanda <afilinov93@gmail.com>
2018-05-30 16:21:05 +0300

    final touch

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -6,7 +6,7 @@ const VideoListItem = ({video, onVideoSelect}) => { //this declares a new variab
     <li onClick={() => onVideoSelect(video)} className="list-group-item">
         <div className="video-list media">
             <div className="media-left">
-                <img className="media-object" src={imageUrl} />>
+                <img className="media-object" src={imageUrl} />
             </div>
 
             <div className="media-body">
